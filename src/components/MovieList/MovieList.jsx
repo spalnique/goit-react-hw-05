cbc2e9d680f2bc5ec7b8189c8de579498bf218a4
spalnique <lex.sydoruk@gmail.com>
2024-04-10 23:07:50 +0200

    upd

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,14 +1,13 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './MovieList.module.css';
 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
   return (
     <ul className={css.movieList}>
       {movies.map(({ id, title, poster_path }) => (
         <li key={id} className={css.movieListItem}>
-          <Link
-            className={css.movieLink}
-            to={`/movies/${id}`}>
+          <Link className={css.movieLink} to={`/movies/${id}`} state={location}>
             <img className={css.moviePoster} src={poster_path} alt={title} />
             <p className={css.movieTitle}>{title}</p>
           </Link>
